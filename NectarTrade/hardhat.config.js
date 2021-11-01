d996fb842291417caabb06a3fd49aa6a44b9ecd8
dogedollar <90071167+TheHoneyPotFinance@users.noreply.github.com>
2021-11-01 18:04:57 +0000

    Deploy Dex Contracts

---
+++ NectarTrade/hardhat.config.js
@@ -0,0 +1,37 @@
+require("@nomiclabs/hardhat-waffle");
+const dotenv = require('dotenv-flow').config();
+const HqDWalletProvider = require("@truffle/hdwallet-provider");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+  arbitrum: {
+    url: "https://arb1.arbitrum.io/rpc",
+    accounts: [process.env.DEPLOYERK]
+  }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+  };
